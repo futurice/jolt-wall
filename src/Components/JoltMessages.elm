4238f237eb7bcf5fcefcbee9f7935b40aa6372f5
Pekka Neuvo <pekka.neuvo@futurice.com>
2018-06-04 23:39:05 +0200

    make jolt feed length more obvious

--- src/Components/JoltMessages.elm
+++ src/Components/JoltMessages.elm
@@ -6,28 +6,33 @@ import List exposing (indexedMap, isEmpty, take)
 import Components.JoltMessage exposing (joltMessage)
 import Types.Msg exposing (..)
 import Types.Model exposing (Model)
+import Types.Message exposing (Message)
+import Types.User exposing (User)
 import Helpers.Jolts exposing (validJolts)
+
+
+joltFeedLength : Int
+joltFeedLength =
+    12
 
 
 joltMessages : Model -> Html Msg
 joltMessages model =
-    let
-        messages =
-            model.flowMessages
+    div [ class "jolt-messages" ] (renderJolts model.flowMessages model.flowUsers)
 
-        renderJolts =
-            if isEmpty messages then
-                [ div [ class "no-jolts" ] [ text "KEINE JOLTS!!! Was ist los!" ] ]
-            else
-                validJolts messages
-                    |> take 12
-                    |> indexedMap
-                        (\index item ->
-                            let
-                                htmlId =
-                                    ("jolt-message-" ++ toString index)
-                            in
-                                div [ id htmlId, class "jolt-message" ] (joltMessage item model.flowUsers)
-                        )
-    in
-        div [ class "jolt-messages" ] renderJolts
+
+renderJolts : List Message -> List User -> List (Html Msg)
+renderJolts messages users =
+    if isEmpty messages then
+        [ div [ class "no-jolts" ] [ text "KEINE JOLTS!!! Was ist los!" ] ]
+    else
+        validJolts messages
+            |> take joltFeedLength
+            |> indexedMap
+                (\index item ->
+                    let
+                        htmlId =
+                            ("jolt-message-" ++ toString index)
+                    in
+                        div [ id htmlId, class "jolt-message" ] (joltMessage item users)
+                )
